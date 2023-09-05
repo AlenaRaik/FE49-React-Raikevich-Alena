ca6ad8378f8d577431823a476f0d3fecbe688601
AlenaRaik <alena.raikevich@gmail.com>
2023-09-05 19:09:41 +0300

    add hw_37

---
+++ home-work-react/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Title } from './ui/title/title';
+import { Menu } from './ui/header/menu';
+
+function App() {
+  return (
+    <div className="App">
+      <Title>Sign In</Title>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ home-work-react/src/ui/title/title.tsx
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+type Props = {
+  children: string;
+};
+
+export const Title: React.FC<Props> = ({ children }) => { 
+    return <TitleWrapper>{children}</TitleWrapper>
+};
+
+const TitleWrapper = styled.h1`
+  font-size: 40px;
+  color: #333;
+`;
